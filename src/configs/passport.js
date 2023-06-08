113d37d6c8305fb773d5647944d385eef9bec960
Nancy Okeke <ogadimmanancy12@gmail.com>
2023-06-08 09:03:43 -0700

    added google-login

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -1,37 +1,31 @@
-// const passport = require("passport");
-// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: "/api/v1/auth/google/callback",
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         // Check if the user already exists
-//         let user = await User.findOne({ email: profile.emails[0].value });
-
-//         if (!user) {
-//           // Create a new user if it doesn't exist
-//           user = await User.create({
-//             fullName: profile.displayName,
-//             email: profile.emails[0].value,
-//             role: "user",
-//           });
-//         }
-
-//         // Generate token and send response
-//         const sendTokenResponse = (user, statusCode, res) => {
-//           const token = user.getSignerJwtToken();
-//           const redirectUrl = `${process.env.FRONTEND_URL}/login?token=${token}`;
-
-//           res.redirect(redirectUrl);
-//         };
-//       } catch (error) {
-//         done(error, false);
-//       }
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      passReqToCallback: true,
+    },
+    async (request, accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      try {
+        // Check if the user already exists
+        let user = await User.findOne({ email: profile.emails[0].value });
+        if (!user) {
+          // Create a new user if it doesn't exist
+          user = await User.create({
+            fullName: profile.displayName,
+            email: profile.emails[0].value,
+            role: "user",
+          });
+        }
+        return done(null, profile);
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
--- src/server.js
+++ src/server.js
@@ -6,8 +6,10 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorMiddlewares");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const authRoute = require("./routes/authRoute");
 const connectDB = require("./configs/databaseConfig");
+const cookieSession = require("cookie-session");
 
 // Initialize Passport
 require("./configs/passport");
@@ -40,7 +42,67 @@ app.use(cors());
 
 app.use("/api/v1/auth", authRoute);
 
+app.set("view engine", "ejs");
+
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: "SECRET",
+//   })
+// );
+
+app.use(
+  cookieSession({
+    name: "tuto-session",
+    keys: ["key1", "key2"],
+  })
+);
+
+var userProfile;
+
 app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+require("./configs/passport");
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+app.get("/success", (req, res) => {
+  res.render("pages/success", {
+    // name: req.user.displayName,
+    pic: req.user.photos[0].value,
+    email: req.user.emails[0].value,
+  });
+});
+app.get("/error", (req, res) => res.send("error logging in"));
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/error" }),
+  function (req, res) {
+    // Successful authentication, redirect success.
+    res.redirect("/success");
+  }
+);
+
+// app.use(passport.initialize());
 const PORT = process.env.PORT || 3001;
 
 app.use(errorHandler);
